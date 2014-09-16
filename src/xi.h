72dd0931f8e0665589c6747e20c0a8344d8e643f
Yu Feng <rainwoodman@gmail.com>
2014-09-16 14:16:32 -0700

    stubs of xi.[ch]

---
+++ src/xi.h
@@ -0,0 +1,12 @@
+
+typedef struct {
+    double pos[3];    
+    double vel[3];
+    double weight;
+} particle;
+
+typedef struct {
+
+} xibindat;
+
+int paircount(dataset * p1, dataset * p2, xibindat b);
