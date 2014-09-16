d6f8942893c7310e181b6dcf8e813d850da1bca2
Beth Reid <beth.ann.reid@rawwwwr.com>
2014-09-16 14:19:05 -0700

    defined xibindat

--- src/xi.h
+++ src/xi.h
@@ -6,7 +6,21 @@ typedef struct {
 } particle;
 
 typedef struct {
-
+  int bintype;
+  int logxopt;
+  int nx;
+  double minx;
+  double dx;
+  int logyopt;
+  int ny;
+  double miny;
+  double dy;
+  int realorzspace;
+  int periodicopt;
+  int nbins2d;
+  //wp only:
+  double rpimax;
+  int zspaceaxis;
 } xibindat;
 
 int paircount(dataset * p1, dataset * p2, xibindat b);
